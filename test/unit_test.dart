7bd925a0b507181857a35e9a8052d06afedd202f
Fabusuyi012 <thedejifab@outlook.com>
2019-07-31 17:45:57 +0100

    minor update

---
+++ test/unit_test.dart
@@ -0,0 +1,10 @@
+import 'package:test_api/test_api.dart';
+void main() {
+  test('Unit test ran successfully', () {
+    // final counter = Counter();
+
+    // counter.increment();
+
+    // expect(counter.value, 1);
+  });
+}
